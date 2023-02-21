28c3ca2d8545e6a089c76331f9d316d683ae1c66
Azhar khan <kazhar136@gmail.com>
2023-02-21 17:03:54 +0530

    location and auth update

--- api/models.py
+++ api/models.py
@@ -13,4 +13,4 @@ class Waste(models.Model):
     location = models.CharField(max_length=50)
     waste_type = models.CharField(max_length=100, choices=type1)
     waste_weight = models.CharField(max_length=50)
-    added_date = models.DateTimeField(auto_now=True)
+    added_date = models.DateField(auto_now_add=False,auto_now=False, blank=True)
--- api/views.py
+++ api/views.py
@@ -3,8 +3,13 @@ from .serializers import WasteSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 
 class WasteAPIView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     def get(self,request,format=None):
         waste = Waste.objects.all()
         serializer = WasteSerializer(waste, many=True)
@@ -12,6 +17,9 @@ class WasteAPIView(APIView):
     
     def post(self,request,format=None):
         serializer = WasteSerializer(data=request.data)
+        for e in Waste.objects.all():
+            if e.location == request.data['location']:
+                return Response({'msg':'This location already exists'})
         if serializer.is_valid():
             serializer.save()
             return Response({'msg':'data created successfully',
